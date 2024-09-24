5f49a526b40f50faa4640cdd105c44ab328e5643
Aman Rajput <rajputaman0000009@gmail.com>
2024-09-24 19:25:10 +0530

    ok

--- script.js
+++ script.js
@@ -46,10 +46,10 @@ ScrollReveal().reveal('.home-img, .service-container, .portfolio-box, .contact f
 ScrollReveal().reveal('.home-contact h1, .about-img',{origin:'left'});
 ScrollReveal().reveal('.home-contact p, .about-content',{origin:'right'});
 
-const typed = new typed('.multiple-text',{
+const typed = new Typed('.multiple-text',{
     strings: ['Frontend Developer ', 'Web Designer', 'Software Engineer'],
     typeSpeed: 70,
     backSpeed: 70,
     backDelay: 1000,
     loop: true,
-})
+});
